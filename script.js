72bea510639e578e495c11c37381738295c4245c
RealMinTech <Adesinamariam7@gmail.com>
2024-11-29 08:03:49 -0800

    Html & css roundup

---
+++ script.js
@@ -0,0 +1,8 @@
+let goodt = document.querySelectorAll(".links");
+
+function openBars() {
+  goodt.forEach((element) => {
+    element.style.display = "block";
+    element.style.color = 'wheat'
+  });
+}
